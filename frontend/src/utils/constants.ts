25d5ad240a044008378d38be47b67f140e57139a
LeeMir <manofgodmj@gmail.com>
2021-11-02 22:29:16 +0900

    [Design] GlobalStyle 적용, 색깔 코드(Enum) 추가
    styled-reset을 GlobalStyle로 적용했습니다.
    색깔 Enum에 LINE3와 PLACEHOLDER를 추가하고, 기존 문자열 형태의 색상을
    색상코드로 변환하였습니다.

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -6,11 +6,13 @@ enum ColorCode {
 	FONT2 = '#ADADAD',
 	LINE1 = '#EBEBEB',
 	LINE2 = '#D1D1D1',
+	LINE3 = '#888888',
 	BACKGROUND1 = '#F5F5F5',
 	BACKGROUND2 = '#F0F0F0',
-	WHITE = 'white',
-	BLACK = 'black',
-	GRAY = 'gray',
+	PLACEHOLDER = '#BBBBBB',
+	WHITE = '#FFFFFF',
+	BLACK = '#000000',
+	GRAY = '#808080',
 	HOVER = '#6264a7',
 }
 
