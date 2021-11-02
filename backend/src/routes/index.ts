34212bb9380626384465654df93f4fef3c9eb126
wonju-dev <wonjulee.dev@gmail.com>
2021-11-03 02:45:19 +0900

    [Refactor] auth Router 코드 리팩토링

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
 	res.send('hello world');
 });
