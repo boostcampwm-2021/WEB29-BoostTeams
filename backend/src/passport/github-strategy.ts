a2300412aa9143851ec36389bedf6500eba337ae
LeeMir <manofgodmj@gmail.com>
2021-11-03 19:27:45 +0900

    [Fix] 일부 오류 개선
    오타 수정 및 오류 개선

--- backend/src/passport/github-strategy.ts
+++ backend/src/passport/github-strategy.ts
@@ -5,7 +5,7 @@ import UserService from '../services/user-service';
 
 const GITHUB_CONFIG = {
 	clientID: process.env.GITHUB_CLIENT_ID,
-	clientSecret: process.env.GITHUB_CLIENT_SECRETE,
+	clientSecret: process.env.GITHUB_CLIENT_SECRET,
 	callbackURL: 'http://localhost:4000/api/auth/github/callback'
 };
 
@@ -13,6 +13,7 @@ async function githubLoginCallback(accessToken, refreshToken, profile, callback)
 	// user_email : id / user_password : node_id / user_name : name
 	const { id, node_id, name } = profile._json;
 	let user = await UserService.getInstance().getUserByEmail(id);
+	console.log(user);
 	if (!user) user = await UserService.getInstance().createUser(id, node_id, name);
 	return callback(null, user);
 }
