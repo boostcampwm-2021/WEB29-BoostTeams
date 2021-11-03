73b88a27f788458849d52c1491f8da23f6fa726c
sycha <sychang0422@gmail.com>
2021-11-03 23:16:56 +0900

    [Chore] dummy data 업데이트

--- frontend/src/components/Calendar/dataStructure.ts
+++ frontend/src/components/Calendar/dataStructure.ts
@@ -1,57 +1,51 @@
+/* eslint-disable camelcase */
 export interface DateInfoType {
 	year: number;
 	month: number;
 	startDate: Date;
 }
 
-export interface Schedule {
+export interface ScheduleType {
 	title: string;
-	startDate: Date;
-	endDate: Date;
-	repeatId: string;
+	start_date: Date;
+	end_date: Date;
+	repeat_id: number;
 	content: string;
 	color: number;
 }
 
-export const scheduleEx = [
+// 월은 0부터 시작
+export const scheduleEx: ScheduleType[] = [
 	{
 		title: '일정1',
-		start_date: new Date(2021, 11, 3, 11, 30),
-		end_date: new Date(2021, 11, 3, 13, 0),
+		start_date: new Date(2021, 10, 3, 11, 30),
+		end_date: new Date(2021, 10, 3, 13, 0),
 		repeat_id: 1,
 		content: '본문1',
 		color: 0,
 	},
 	{
 		title: '일정2',
-		start_date: new Date(2021, 11, 5, 15, 30),
-		end_date: new Date(2021, 11, 5, 18, 30),
+		start_date: new Date(2021, 10, 5, 15, 30),
+		end_date: new Date(2021, 10, 5, 18, 30),
 		repeat_id: 2,
 		content: '본문2',
 		color: 1,
 	},
 	{
 		title: '일정3',
-		start_date: new Date(2021, 11, 4, 15, 30),
-		end_date: new Date(2021, 11, 4, 17, 0),
+		start_date: new Date(2021, 10, 3, 15, 15),
+		end_date: new Date(2021, 10, 3, 17, 0),
 		repeat_id: 1,
 		content: '본문3',
 		color: 0,
 	},
 	{
 		title: '일정4',
-		start_date: new Date(2021, 11, 1, 9, 30),
-		end_date: new Date(2021, 11, 1, 11, 0),
+		start_date: new Date(2021, 10, 1, 9, 30),
+		end_date: new Date(2021, 10, 1, 11, 0),
 		repeat_id: 1,
 		content: '본문4',
-		color: 0,
-	},
-	{
-		title: '일정5',
-		start_date: new Date(2021, 11, 4, 9, 30),
-		end_date: new Date(2021, 11, 4, 10, 0),
-		repeat_id: 1,
-		content: '본문5',
 		color: 0,
 	},
 ];
