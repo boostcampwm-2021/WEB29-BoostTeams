cfc28772c287351de315d875af236286b9700ece
LeeMir <manofgodmj@gmail.com>
2021-11-04 21:24:49 +0900

    [Feat] 회원가입 API 작성, 암호화
    /api/auth/signup에 대한 controller를 수정하고, 닉네임 중복을 검사하기
    위해 service에 getUserByName을 추가하였습니다.
    
    일부 function으로 작성된 함수들을 arrow function으로 바꿨으며, JWT
    token을 검사하는 로직에 undefined도 검사하게 추가하였습니다.
    
    redirect와 send를 구분하기 위해 githubLogin을 따로 분리했습니다.
    
    회원가입 및 로그인 시 비밀번호 암호화를 진행합니다.
    - FE에서 AES로 암호화된 값을 복호화한 뒤, bcrypt로 암호화해서
      진행합니다.

--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -1,5 +1,7 @@
 import { getCustomRepository } from 'typeorm';
 import UserRepository from '../repositories/user-repository';
+import Crypto from 'crypto-js';
+import bcrypt from 'bcrypt';
 
 class UserService {
 	static instance: UserService;
@@ -27,7 +29,9 @@ class UserService {
 		return { user_id, user_email, user_name, user_state };
 	}
 
-	async createUser(user_email: string, user_password: string, user_name: string) {
+	async createUser(user_email: string, encryptedPassword: string, user_name: string) {
+		const decryptedPassword = Crypto.AES.decrypt(encryptedPassword, process.env.AES_KEY).toString();
+		const user_password = bcrypt.hashSync(decryptedPassword, Number(process.env.SALT_OR_ROUNDS));
 		const newUser = await this.userRepository.save({ user_email, user_password, user_name, user_state: 0 });
 		return newUser;
 	}
@@ -36,6 +40,15 @@ class UserService {
 		const user = await this.userRepository.findOne({
 			where: {
 				user_email: user_email
+			}
+		});
+		return user;
+	}
+
+	async getUserByName(user_name: string) {
+		const user = await this.userRepository.findOne({
+			where: {
+				user_name: user_name
 			}
 		});
 		return user;
