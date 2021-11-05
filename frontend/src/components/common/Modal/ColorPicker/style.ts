a5a4d08b5a8df747f145e14dfd8e5507bb004898
Kang Minji <mminjg@gmail.com>
2021-11-06 00:34:07 +0900

    [Feat] color picker 드롭다운 구현
    재사용 가능한 색상을 선택하는 드롭다운을 구현하였습니다.

---
+++ frontend/src/components/common/Modal/ColorPicker/style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { ColorCode } from '../../../../utils/constants';
+
+export const Container = styled.div`
+	position: relative;
+	width: 1.2rem;
+	height: 1.2rem;
+`;
+
+export const ColorDropDown = styled.div`
+	position: absolute;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+	top: 1.8rem;
+	left: -0.7rem;
+	width: 12rem;
+	height: 2rem;
+	background-color: ${ColorCode.WHITE};
+	border-radius: 8px;
+	box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+`;
+export const ColorCircle = styled.div`
+	width: 1.2rem;
+	height: 1.2rem;
+	border-radius: 50%;
+	background-color: ${(props) => props.color};
+	cursor: pointer;
+`;
