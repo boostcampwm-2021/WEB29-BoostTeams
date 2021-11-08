40ee29fdf5f0a8477039d8d9b7172d2716a237ed
wonju-dev <wonjulee.dev@gmail.com>
2021-11-08 23:12:33 +0900

    [Feat] 팀 선택 page template 구현

--- frontend/src/templates/TeamTemplate/index.tsx
+++ frontend/src/templates/TeamTemplate/index.tsx
@@ -5,17 +5,26 @@ import { CardList } from '../../components/Team';
 
 import { Layout } from './style';
 
+const myTeam = [
+	{ team_id: 0, team_name: 'team#1', team_desc: 'team_desc#1' },
+	{ team_id: 1, team_name: 'team#2', team_desc: 'team_desc#2' },
+	{ team_id: 2, team_name: 'team#3', team_desc: 'team_desc#3' },
+];
+
+const inviteTeam = [{ team_id: 3, team_name: 'team#4', team_desc: 'team_desc#4' }];
+
 const Team: React.FC = () => {
 	return (
 		<Layout>
 			<Header />
 			<Suspense fallback={<div>loading</div>}>
-				<CardList type='myTeam' />
+				<CardList dummy={myTeam} type='myTeam' />
 			</Suspense>
 			<Suspense fallback={<div>loading</div>}>
-				<CardList type='invitation' />
+				<CardList dummy={inviteTeam} type='invitation' />
 			</Suspense>
 		</Layout>
 	);
 };
+
 export default Team;
