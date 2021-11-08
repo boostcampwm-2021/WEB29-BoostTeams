9caa65cb45919aa625efe6823f8eda0c265f98da
wonju-dev <wonjulee.dev@gmail.com>
2021-11-09 02:41:07 +0900

    [Feat] 팀 생성 API 처리 로직 구성

--- backend/src/controllers/team-user-contorller.ts
+++ backend/src/controllers/team-user-contorller.ts
@@ -11,6 +11,33 @@ const TeamUserController = {
 		} catch (err) {
 			res.status(400).send(err);
 		}
+	},
+
+	async createTeams(req: Request, res: Response) {
+		try {
+			const userId = parseUserId(req.path);
+			await TeamUserService.getInstance().createNewTeam(userId, req.body);
+			res.status(200).send('creation success');
+		} catch (err) {
+			res.status(400).send(err);
+		}
+	},
+
+	async updateTeam(req: Request, res: Response) {
+		try {
+			const userId = parseUserId(req.path);
+			const teamId = parseTeamId(req.path);
+		} catch (err) {
+			res.status(400).send(err);
+		}
+	},
+	async deleteTeam(req: Request, res: Response) {
+		try {
+			const userId = parseUserId(req.path);
+			const teamId = parseTeamId(req.path);
+		} catch (err) {
+			res.status(400).send(err);
+		}
 	}
 };
 
--- backend/src/services/team-user-service.ts
+++ backend/src/services/team-user-service.ts
@@ -1,6 +1,11 @@
 import { getCustomRepository } from 'typeorm';
 import TeamUserRepository from '../repositories/team-user-repository';
+import TeamRepository from '../repositories/team-repository';
 
+interface teamInfo {
+	team_name: string;
+	team_desc: string;
+}
 export default class TeamUserService {
 	static instance: TeamUserService;
 	teamUserRepository: TeamUserRepository;
@@ -23,5 +28,27 @@ export default class TeamUserService {
 			.where('teamUser.user = :userId', { userId })
 			.getMany();
 		return teams;
+	}
+
+	async createNewTeam(userId: number, newTeam: teamInfo) {
+		// team Table에 데이터 삽입
+		// team-user-service에서 분리해야 함
+		const insertResult = await getCustomRepository(TeamRepository)
+			.createQueryBuilder('team')
+			.insert()
+			.into('team')
+			.values(newTeam)
+			.execute();
+		const newTeamId = insertResult.raw.insertId;
+		// team-user Table에 데이터 삽입
+		await this.teamUserRepository
+			.createQueryBuilder('team_user')
+			.insert()
+			.into('team_user')
+			.values({
+				user: userId,
+				team: newTeamId
+			})
+			.execute();
 	}
 }
