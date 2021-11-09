f0f40d3d11ae673a0092159f2af513a0e3da3f75
Kang Minji <mminjg@gmail.com>
2021-11-10 01:47:03 +0900

    [Fix] 일정 가져오기 api 수정
    쿼리 빌더로 변경하였고, teamId로 선별하여 가져오도록 수정하였습니다.

--- backend/src/services/schedule-service.ts
+++ backend/src/services/schedule-service.ts
@@ -28,33 +28,50 @@ class ScheduleService {
 		const schedules = [];
 		const repeat_id = uuidv4();
 		const { title, team_id, start_date, end_date, repeat_option, repeat_count, content, color } = scheduleInfo;
-		[...Array(Number(repeat_count))].forEach((v, i) => {
-			const startDate = moment(new Date(start_date)).add(i, addOption[repeat_option]).toString();
-			const endDate = moment(new Date(end_date)).add(i, addOption[repeat_option]).toString();
-			schedules.push({
-				title,
-				team_id,
-				start_date: startDate,
-				end_date: endDate,
-				content,
-				color,
-				repeat_id,
-				repeat_option
-			});
+		schedules.push({
+			title,
+			team_id,
+			start_date: moment(start_date).toDate(),
+			end_date: moment(end_date).toDate(),
+			content,
+			color,
+			repeat_id,
+			repeat_option
 		});
+		if (repeat_option !== 0) {
+			[...Array(repeat_count - 1)].forEach((v, i) => {
+				const startDate = moment(new Date(start_date))
+					.add(i + 1, addOption[repeat_option])
+					.toDate();
+				const endDate = moment(new Date(end_date))
+					.add(i + 1, addOption[repeat_option])
+					.toDate();
+				schedules.push({
+					title,
+					team_id,
+					start_date: startDate,
+					end_date: endDate,
+					content,
+					color,
+					repeat_id,
+					repeat_option
+				});
+			});
+		}
 		const resultSchedules = await Promise.all(
 			schedules.map(async (schedule) => this.scheduleRepository.save(schedule))
 		);
-		console.log(resultSchedules);
 		if (!resultSchedules) throw new Error('일정 생성 실패');
 		return resultSchedules;
 	}
 
 	async getSchedule(teamId, startDate, endDate) {
-		const schedules = await this.scheduleRepository.find({
-			where: { team_id: teamId, start_date: MoreThan(startDate), end_date: LessThan(endDate) },
-			relations: ['team']
-		});
+		const schedules = await this.scheduleRepository
+			.createQueryBuilder('schedule')
+			.where('schedule.team_id = :teamId', { teamId })
+			.andWhere('schedule.start_date >= :startDate ', { startDate })
+			.andWhere('schedule.end_date <= :endDate', { endDate })
+			.getMany();
 		if (!schedules) throw new Error('일정 가져오기 실패');
 		return schedules;
 	}
