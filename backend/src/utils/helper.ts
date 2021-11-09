92b0cd46645eedf3dfaf78e06ee893039192a3e1
wonju-dev <wonjulee.dev@gmail.com>
2021-11-09 18:32:38 +0900

    [Refactor] 미사용 helper function 제거

--- backend/src/utils/helper.ts
+++
@@ -1,5 +0,0 @@
-export const parseId = (path: string) => {
-	const regex = /\/(?<userId>[\d]*)(\/teams\/)?(?<teamId>[\d]*)?/;
-	const userId = regex.exec(path).groups;
-	return Object.values(userId).map((value) => Number(value));
-};
