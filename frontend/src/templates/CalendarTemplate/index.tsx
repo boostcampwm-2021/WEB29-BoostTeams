4b9efd50d6a47693cf5e7eb18276da7b0b78d255
Kang Minji <mminjg@gmail.com>
2021-11-09 11:01:21 +0900

    [refactor] 필요없는 hook 제거, useState로 변경

--- frontend/src/templates/CalendarTemplate/index.tsx
+++ frontend/src/templates/CalendarTemplate/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { getFirstDate, getLastDate, getCurrDateInfo, getPrevDateInfo, getNextDateInfo } from '../../utils/calendar';
-import { useDate } from '../../hooks/schedule';
 import UserState from '../../stores/user';
 import { getSchedules } from '../../apis/schedule';
 import { ScheduleType } from '../../components/Calendar/dataStructure';
@@ -15,10 +14,10 @@ import CalendarModal from '../../components/Calendar/CalendarModal';
 import { Layout, MainContainer, CalendarContainer } from './style';
 
 const Calendar: React.FC = () => {
-	const [isMonthly, setIsMonthly] = useState<boolean>(false);
 	const [schedules, setSchedules] = useState<ScheduleType[]>([]);
-	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-	const [dateInfo, setDateInfo] = useDate();
+	const [dateInfo, setDateInfo] = useState(getCurrDateInfo());
+	const [isMonthly, setIsMonthly] = useState(false);
+	const [isModalVisible, setIsModalVisible] = useState(false);
 
 	const teamId = useRecoilValue(UserState).team_id;
 	const firstDate = getFirstDate(isMonthly, dateInfo).format('YYYYMMDD');
@@ -48,11 +47,6 @@ const Calendar: React.FC = () => {
 		const { year, month, weeklyStartDate } = dateInfo;
 		setDateInfo(getNextDateInfo(year, month, weeklyStartDate, isMonthly));
 	};
-	// 월 -> 주 넘어갈 때 그 월의 첫째주를 보여준다?
-	// 원래 10월 24일이 startdate인 주를 보고 있었는데
-	// 월로 넘긴 다음에 11월로 바꾸고 다시 주로 돌아온다면
-	// 11월 첫째주가 보여지는데..
-	// year: month: weeklyStartDate: 첫째주의 일요일 date
 
 	useEffect(() => {
 		fetchSchedules();
