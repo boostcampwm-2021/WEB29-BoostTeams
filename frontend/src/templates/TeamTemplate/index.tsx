2bbb06c249ae2a785391e5110bbb72d8e03d61bd
LeeMir <manofgodmj@gmail.com>
2021-11-11 01:32:43 +0900

    [Refactor] Team Template 수정
    Card와 CardList의 분리 및 상태의 변경에 따라 수정하였습니다.
    원래는 모든 상태를 Page에서 관리해야하나, recoil의 selector에서 api를
    호출하는 것때문에 Suspense를 강제해 recoil은 template로 옮겼습니다.
    
    아직 아무 기능이 없으나 띄워지는 걸 확인하기 위해 CreateModal을 추가했습니다.

--- frontend/src/templates/TeamTemplate/index.tsx
+++ frontend/src/templates/TeamTemplate/index.tsx
@@ -1,19 +1,36 @@
-import React, { Suspense } from 'react';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { userTeamList } from '../../stores/team';
 
+import { CardData } from '../../components/Team/type';
 import { Header } from '../../components/common';
-import { CardList } from '../../components/Team';
+import { MyTeamList, InviteList, CreateTeamModal } from '../../components/Team';
 import { Layout } from './style';
 
-const TeamTemplate: React.FC = () => {
+type Props = {
+	showCreateTeamModal: boolean;
+	handleModalOpen: () => void;
+	handleModalClose: () => void;
+};
+
+const TeamTemplate: React.FC<Props> = ({ showCreateTeamModal, handleModalOpen, handleModalClose }) => {
+	const teamList = useRecoilValue(userTeamList);
+	const myTeamList: CardData[] = [];
+	const inviteList: CardData[] = [];
+	teamList.reduce((pre: void, team: CardData) => {
+		if (team.state) {
+			myTeamList.push(team);
+		} else {
+			inviteList.push(team);
+		}
+		return pre;
+	}, '');
 	return (
 		<Layout>
 			<Header />
-			<Suspense fallback={<div>loading</div>}>
-				<CardList type='myTeam' />
-			</Suspense>
-			<Suspense fallback={<div>loading</div>}>
-				<CardList type='invitation' />
-			</Suspense>
+			<MyTeamList list={myTeamList} handleModalOpen={handleModalOpen} />
+			<InviteList list={inviteList} />
+			{showCreateTeamModal && <CreateTeamModal handleModalClose={handleModalClose} />}
 		</Layout>
 	);
 };
