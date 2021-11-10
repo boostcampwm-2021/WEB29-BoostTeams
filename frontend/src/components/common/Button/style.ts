6d3ef4eb446530cce1484028ca8463f5260f985a
LeeMir <manofgodmj@gmail.com>
2021-11-11 01:37:35 +0900

    [Design] common button hover 수정
    opacity가 아닌, filter로 밝기를 조절하였습니다.

--- frontend/src/components/common/Button/style.ts
+++ frontend/src/components/common/Button/style.ts
@@ -18,7 +18,7 @@ export const NormalButton = styled.button<NormalButtonProps>`
 	font-weight: 500;
 	padding: 0.2rem 1rem;
 	cursor: pointer;
-	:hover {
-		opacity: 0.9;
+	&:hover {
+		filter: brightness(120%);
 	}
 `;
