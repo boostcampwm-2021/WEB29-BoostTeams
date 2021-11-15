10566c977360f684c83309c6efa3b177da5806eb
Kang Minji <mminjg@gmail.com>
2021-11-16 01:04:33 +0900

    [Fix] 새로고침 했을 때 소켓, userEmail을 가져오지 못하는 문제 해결
    TeamRoute useEffect의 종속 배열에 userEmail을 넣어주었습니다. UsersPage도 socketRef.current를 종속 배열에 넣어 해결하였습니다.

--- frontend/src/pages/UsersPage/index.tsx
+++ frontend/src/pages/UsersPage/index.tsx
@@ -5,15 +5,17 @@ import { SocketContext } from '@utils/socketContext';
 const UsersPage: React.FC = () => {
 	const socketRef = useContext(SocketContext);
 	useEffect(() => {
-		socketRef.current.on('online users', (data: any) => {
-			console.log(data);
-		});
-		socketRef.current.emit('enter users room');
+		if (socketRef.current) {
+			socketRef.current.on('online users', (data: any) => {
+				console.log(data);
+			});
+			socketRef.current.emit('enter users room');
+		}
 		return () => {
 			socketRef.current.emit('leave users room');
 			socketRef.current.off('online users');
 		};
-	}, []);
+	}, [socketRef.current]);
 
 	return <UsersTemplate />;
 };
