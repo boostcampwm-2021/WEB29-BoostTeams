6dd7a57178e61f9e1a6503c8d90fe1555bf6c529
LeeMir <manofgodmj@gmail.com>
2021-11-15 14:39:53 +0900

    [Fix] config-overrides 추가
    eject 대신
    customize-cra 설치
    react-app-rewired 설치
    config-overrides 작성

---
+++ frontend/config-overrides.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { override, addWebpackAlias } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+	addWebpackAlias({
+		'@src': path.resolve(__dirname, 'src'),
+		'@apis': path.resolve(__dirname, 'src/apis'),
+		'@components': path.resolve(__dirname, 'src/components'),
+		'@pages': path.resolve(__dirname, 'src/pages'),
+		'@routes': path.resolve(__dirname, 'src/routes'),
+		'@stores': path.resolve(__dirname, 'src/stores'),
+		'@styles': path.resolve(__dirname, 'src/styles'),
+		'@templates': path.resolve(__dirname, 'src/templates'),
+		'@utils': path.resolve(__dirname, 'src/utils'),
+	}),
+);
