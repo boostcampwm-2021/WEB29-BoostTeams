4cf0ee007495fcd4ccf54b3871f833021d696f66
wonju-dev <wonjulee.dev@gmail.com>
2021-11-17 02:01:25 +0900

    [Feat] BE, 팀 보드 관련 socket 로직 구현
    1. 팀 보드 입장
    2. 포스트잇 생성
    3. 포스트잇 이동

--- backend/src/sockets/team.ts
+++ backend/src/sockets/team.ts
@@ -48,13 +48,78 @@ const teamInit = (namespace: Namespace): void => {
 			socket.leave('users');
 		});
 
+		socket.on('join board page', () => {
+			console.log('join board page');
+			socket.join('board');
+			socket.emit('join board page', dummyPostit);
+		});
+
+		socket.on('leave board page', () => {
+			console.log('leave board page');
+			socket.leave('board');
+		});
+
+		socket.on('create new postit', ({ title, desc, teamId }) => {
+			console.log(title, desc, teamId);
+			// 새로운 post 생성 & 저장
+			const newPostit = makeNewPostit(title, desc, teamId);
+			dummyPostit.push(newPostit);
+			// 전체 post를 생성자한테 전송
+			socket.emit('create new postit', dummyPostit);
+			// 전체 post를 room에 broadcast 전송
+			socket.broadcast.to('board').emit('create new postit', dummyPostit);
+		});
+
+		socket.on('drag postit', ({ id, x, y }) => {
+			const updatedPostit = dummyPostit.filter((postit) => {
+				if (postit.key !== id) return postit;
+				else {
+					const temp = postit;
+					temp.x = x;
+					temp.y = y;
+					return temp;
+				}
+			});
+			socket.broadcast.to('board').emit('drag postit', updatedPostit);
+		});
+
 		socket.on('disconnect', () => {
 			const { teamId, userId } = onlineUsersInfo[socket.id];
 			setUserStatusToOffline(teamId, userId, socket.id);
 			sendOnlineUsersToRoom(socket, teamId);
-			// console.log('offline', onlineUsersByTeam, onlineUsersInfo);
 		});
 	});
 };
 
 export default teamInit;
+/* 
+{
+	team#1 : [{post#1}, {post#2}],
+	team#2 : [{post#1}, {post#2}, {post#3}]
+}
+*/
+
+const makeNewPostit = (title, desc, teamId) => {
+	return {
+		key: dummyPostit.length + 1,
+		title: title,
+		content: desc,
+		x: 0,
+		y: 0,
+		color: 'red',
+		updatedDate: JSON.stringify(new Date())
+	};
+};
+
+const dummyPostit = [
+	{
+		key: 1,
+		title: 'title#1',
+		content:
+			'desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1desc#1',
+		x: 10,
+		y: 10,
+		color: 'red',
+		updatedDate: '2021.07.02.'
+	}
+];
