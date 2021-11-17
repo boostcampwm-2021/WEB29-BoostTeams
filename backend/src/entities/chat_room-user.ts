5295b8c427805c952f4869367577701b6ac92e16
Kang Minji <mminjg@gmail.com>
2021-11-17 15:21:37 +0900

    [Fix] chat_room-user 엔티티 수정
    user id 참조를 team user에서 user로 변경하였습니다.

--- backend/src/entities/chat_room-user.ts
+++ backend/src/entities/chat_room-user.ts
@@ -1,15 +1,15 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { ChatRoom } from './chat_room';
-import { TeamUser } from './team-user';
+import { User } from './user';
 
 @Entity({ name: 'chat_room_user' })
 export class ChatRoomUser {
 	@PrimaryGeneratedColumn()
 	chat_room_user_id!: number;
 
-	@ManyToOne(() => TeamUser, (teamUser) => teamUser.team_user_id)
+	@ManyToOne(() => User, (user) => user.user_id)
 	@JoinColumn({ name: 'user_id' })
-	team_user: TeamUser;
+	user: User;
 
 	@ManyToOne(() => ChatRoom, (chatRoom) => chatRoom.chat_room_id)
 	@JoinColumn({ name: 'chat_room_id' })
