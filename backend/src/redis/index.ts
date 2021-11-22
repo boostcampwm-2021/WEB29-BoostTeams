f986ec67c94a39cff827e27c5e6bc14ffd19467e
wonju-dev <wonjulee.dev@gmail.com>
2021-11-22 16:27:20 +0900

    [Feat] Redis API 구성

--- backend/src/redis/index.ts
+++ backend/src/redis/index.ts
@@ -17,10 +17,43 @@ export default class Redis {
 		}
 		return Redis.instance;
 	}
-	get(key: string) {
-		console.log('redis get');
+	// key === 'board' || 'chat'
+	// field === roomId || chatId
+	async get(key: string, field: string) {
+		return new Promise((resolve, reject) => {
+			Redis.client.hget(key, field, (err, obj) => {
+				if (err) reject(err);
+				else resolve(obj);
+			});
+		});
 	}
-	set(key: string, value: any) {
-		console.log('redis set');
+
+	async set(key: string, field: string, value: any) {
+		return new Promise((resolve, reject) => {
+			Redis.client.hget(key, field, (err, obj) => {
+				if (err) reject(err);
+				else {
+					const updatedValue = [...JSON.parse(obj), value];
+					Redis.client.hset(key, field, JSON.stringify(updatedValue), () => {
+						resolve(updatedValue);
+					});
+				}
+			});
+		});
 	}
 }
+
+/* 
+client = {
+	board : {
+		team#1 : [{postIt#1-1},{postIt#1-2},{postIt#1-3}],
+		team#2 : [{postIt#2-1},{postIt#2-2},{postIt#2-3}],
+		team#3 : [{postIt#3-1},{postIt#3-2},{postIt#3-3}],
+	},
+	chat : {
+		room#1 : [{chat#1-1},{chat#1-2}],
+		room#2 : [{chat#2-1},{chat#2-2}],
+		room#3 : [{chat#3-1},{chat#3-2}],
+	},
+}
+*/
