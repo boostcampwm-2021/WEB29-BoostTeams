3afd2a2e7d2791e01bc91c4bd9c17d2a6a75923e
wonju-dev <wonjulee.dev@gmail.com>
2021-11-22 23:54:02 +0900

    [Refactor] 포스트잇 렌더링 관련 오류 수정, socket API 리팩토링
    1. Redis 도입 이후 발생한 포스트잇 랜더링 관련 오류를 수정했습니다.
    2. socket API를 리팩토링 했습니다. (개선 필요)
    3. toastify를 이용해서 오류 toast message를 구현했습니다.

--- frontend/src/pages/BoardPage/index.tsx
+++ frontend/src/pages/BoardPage/index.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { SocketContext } from '@utils/socketContext';
 import BoardTemplate from '@templates/BoardTemplate';
-import userState from '@stores/user';
-import { useRecoilValue } from 'recoil';
+import { toast } from 'react-toastify';
 
 export interface PostitType {
 	id: number;
@@ -18,8 +17,6 @@ export interface PostitType {
 }
 
 const BoardPage: React.FC = () => {
-	const user = useRecoilValue(userState);
-
 	const [postits, setPostits] = useState<PostitType[]>([]);
 	const [showModal, setShowModal] = useState(false);
 	const [modalType, setModalType] = useState('create');
@@ -31,30 +28,30 @@ const BoardPage: React.FC = () => {
 	const socketApi = {
 		createNewPostit: (newPostit: PostitType) => socket.current.emit('create new postit', newPostit),
 		updatePostit: (newPostit: PostitType) => socket.current.emit('update postit', newPostit),
+		initPostitState: (initPoistList: PostitType[]) => setPostits(initPoistList),
 		dragPostit: (e: any) => {
 			const id = e.target.id();
 			const x = e.target.x();
 			const y = e.target.y();
 			socket.current.emit('drag postit', { id, x, y });
 		},
-	};
-
-	const updatePostits = (newPostit: PostitType) => {
-		setPostits((prev: PostitType[]) => {
-			const newPostits = [...prev!];
-			const postitIdx = newPostits.findIndex((elem) => Number(newPostit.id) === elem.id);
-			newPostits.splice(postitIdx, 1);
-			return [...newPostits, newPostit];
-		});
+		setPostitState: (newPostit: PostitType) => {
+			setPostits((previousPostitList: PostitType[]) => {
+				const postitIdx = previousPostitList.findIndex((elem) => Number(newPostit.id) === Number(elem.id));
+				if (postitIdx !== -1) previousPostitList.splice(postitIdx, 1);
+				return [...previousPostitList, newPostit];
+			});
+		},
 	};
 
 	useEffect(() => {
 		if (socket.current) {
 			socket.current.emit('join board page');
-			socket.current.on('join board page', (postit: any) => setPostits(postit));
-			socket.current.on('create new postit', (postit: any) => setPostits(postit));
-			socket.current.on('update postit', (postit: PostitType) => updatePostits(postit));
-			socket.current.on('drag postit', (postit: PostitType) => updatePostits(postit));
+			socket.current.on('join board page', (postit: PostitType[]) => socketApi.initPostitState(postit));
+			socket.current.on('create new postit', (postit: PostitType) => socketApi.setPostitState(postit));
+			socket.current.on('update postit', (postit: PostitType) => socketApi.setPostitState(postit));
+			socket.current.on('drag postit', (postit: PostitType) => socketApi.setPostitState(postit));
+			socket.current.on('team board error', (message?: string) => toast.error(message));
 		}
 		return () => {
 			socket.current.emit('leave board page');
@@ -62,6 +59,7 @@ const BoardPage: React.FC = () => {
 			socket.current.off('create new postit');
 			socket.current.off('update postit');
 			socket.current.off('drag postit');
+			socket.current.off('team board error');
 		};
 	}, [socket]);
 
