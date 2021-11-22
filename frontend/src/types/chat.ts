4c5548098146d7e1dbd21dff88ff978d8f25c6fb
Kang Minji <mminjg@gmail.com>
2021-11-23 01:39:36 +0900

    [Style] 사용하지 않는 코드 제거

--- frontend/src/types/chat.ts
+++ frontend/src/types/chat.ts
@@ -40,12 +40,3 @@ export interface ChatRoomResType {
 	chat_room_id: number;
 	chat_room_name: string;
 }
-
-export const messagesEx = [
-	{ messageId: 1, userId: 1, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-	{ messageId: 2, userId: 2, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-	{ messageId: 3, userId: 3, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-	{ messageId: 4, userId: 4, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-	{ messageId: 5, userId: 1, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-	{ messageId: 6, userId: 2, content: 'hi', createdAt: new Date(), chatRoomId: 1 },
-];
