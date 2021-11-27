2eec596a7c07a86cb18962e97da20a287a62e548
wonju-dev <wonjulee.dev@gmail.com>
2021-11-27 15:28:23 +0900

    [Refactor] teamBoard socket Event 종류를 상수 별도로 관리

---
+++ backend/src/sockets/eventType.ts
@@ -0,0 +1,25 @@
+const boardEvents = {
+	ENTER_PAGE: 'join board page',
+	LEAVE_PAGE: 'leave board page',
+	CREATE_POSTIT: 'create new postit',
+	UPDATE_START: 'update start postit',
+	UPDATE_END: 'update end postit',
+	DRAG_START: 'drag postit',
+	DRAG_END: 'drag end postit',
+	DELETE: 'delete postit',
+	ERROR: {
+		TYPE: 'team board error',
+		MESSAGES: {
+			LOAD: '포스트잇을 불러오는데 실패했습니다!',
+			CREATE: '새로운 포스트잇 생성 실패!',
+			UPDATE: '포스트잇을 업데이트 할 수 없습니다!',
+			DELETE: '포스트잇 삭제 실패!'
+		}
+	}
+};
+
+const teamEvents = {};
+
+const chatEvents = {};
+
+export { boardEvents, teamEvents, chatEvents };
