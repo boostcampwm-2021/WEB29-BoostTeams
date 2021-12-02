b1d1ec98a228764346bb8077c22c4c8ff2d2b9ac
LeeMir <manofgodmj@gmail.com>
2021-12-02 20:23:17 +0900

    [Refactor] Font 최적화
    font 불러오는 것을 styled-components => css로 변경
    woff2 타입 추가
    NotoSans 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import React, { Suspense } from 'react';
 import dotenv from 'dotenv';
 import { RecoilRoot } from 'recoil';
 import { ToastContainer } from 'react-toastify';
-import Router from './routes/router';
+import Router from '@routes/router';
+import LoadingPage from '@pages/LoadingPage';
+import '@styles/fonts.css';
 import 'react-toastify/dist/ReactToastify.css';
-import LoadingPage from './pages/LoadingPage';
 
 dotenv.config();
 
