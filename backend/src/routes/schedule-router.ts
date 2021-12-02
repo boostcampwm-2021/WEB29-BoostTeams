684f1945147e498f6e173a3d1362f8ddd942ef5d
LeeMir <manofgodmj@gmail.com>
2021-12-02 11:56:25 +0900

    [Fix] 일정 불러올 때 권한 없음 오류
    JWT token 인증 middleware가 빠져있었음

--- backend/src/routes/schedule-router.ts
+++ backend/src/routes/schedule-router.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import ScheduleController from '@controllers/schedule-controller';
 import { checkTeamUser } from '@src/middlewares/user';
+import { authenticateToken } from '@src/middlewares/token';
 
 const router = express.Router();
 
-router.post('/:teamId', checkTeamUser, ScheduleController.createSchedule);
-router.get('/:teamId', checkTeamUser, ScheduleController.getSchedule);
+router.post('/:teamId', authenticateToken, checkTeamUser, ScheduleController.createSchedule);
+router.get('/:teamId', authenticateToken, checkTeamUser, ScheduleController.getSchedule);
 router.put('/:scheduleId', ScheduleController.updateRepeatSchedule);
 router.delete('/:scheduleId', ScheduleController.deleteSchedule);
 
