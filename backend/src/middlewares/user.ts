10d8804cda46bb1790f50f654c0bcaa6793dc0c5
wonju-dev <wonjulee.dev@gmail.com>
2021-12-20 17:00:33 +0900

    [Refactor] typeORM 레포지토리 리펙토링에 따른 user middleware 수정

--- backend/src/middlewares/user.ts
+++ backend/src/middlewares/user.ts
@@ -1,27 +1,31 @@
 import { Response, NextFunction } from 'express';
-import UserService from '@services/user-service';
-import TeamUserService from '@src/services/team-user-service';
+import { getCustomRepository } from 'typeorm';
+import UserRepository from '@src/repositories/user-repository';
+import TeamUserRepository from '@src/repositories/team-user-repository';
 
 export const getUserInfo = async (req: any, res: Response, next: NextFunction) => {
+	const userRepository = getCustomRepository(UserRepository);
 	const user_id = req.user_id;
-	const user = await UserService.getInstance().getUser(user_id);
+	const user = await userRepository.getUser(user_id);
 	if (!user) return res.status(401).send({ msg: 'user not found' });
 	req.user = user;
 	next();
 };
 
 export const checkTeamUser = async (req: any, res: Response, next: NextFunction) => {
+	const teamUserRepository = getCustomRepository(TeamUserRepository);
 	const userId = req.user_id;
 	const teamId = req.params.teamId;
-	const teamUser = await TeamUserService.getInstance().checkTeamUser(teamId, userId);
+	const teamUser = await teamUserRepository.checkTeamUser(teamId, userId);
 	if (!teamUser) return res.status(403).send({ msg: 'you are not member!' });
 	next();
 };
 
 export const checkIsManager = async (req: any, res: Response, next: NextFunction) => {
+	const teamUserRepository = getCustomRepository(TeamUserRepository);
 	const userId = req.user_id;
 	const teamId = req.params.teamId;
-	const teamUser = await TeamUserService.getInstance().checkTeamUser(teamId, userId);
+	const teamUser = await teamUserRepository.checkTeamUser(teamId, userId);
 	if (!teamUser || teamUser.role !== 0) return res.status(403).send({ msg: 'you are not manager!' });
 	next();
 };
